51c5e2d765d1ca13234a01fc994ac4e1bb262e30
Marawan Ragab <marawan31@gmail.com>
2018-09-26 15:30:30 -0400

    fixed urls.py

--- dmoj/urls.py
+++ dmoj/urls.py
@@ -23,58 +23,58 @@ from judge.views.select2 import UserSelect2View, OrganizationSelect2View, Proble
 
 admin.autodiscover()
 
-register_patterns = [
-    url(r'^activate/complete/$',
-        TitledTemplateView.as_view(template_name='registration/activation_complete.html',
-                                   title='Activation Successful!'),
-        name='registration_activation_complete'),
-    # Activation keys get matched by \w+ instead of the more specific
-    # [a-fA-F0-9]{40} because a bad activation key should still get to the view;
-    # that way it can return a sensible "invalid key" message instead of a
-    # confusing 404.
-    url(r'^activate/(?P<activation_key>\w+)/$',
-        ActivationView.as_view(title='Activation key invalid'),
-        name='registration_activate'),
-    url(r'^register/$',
-        RegistrationView.as_view(title='Register'),
-        name='registration_register'),
-    url(r'^register/complete/$',
-        TitledTemplateView.as_view(template_name='registration/registration_complete.html',
-                                   title='Registration Completed'),
-        name='registration_complete'),
-    url(r'^register/closed/$',
-        TitledTemplateView.as_view(template_name='registration/registration_closed.html',
-                                   title='Registration not allowed'),
-        name='registration_disallowed'),
-    url(r'^login/$', auth_views.LoginView.as_view(
-        template_name='registration/login.html',
-        extra_context={'title': _('Login')},
-        authentication_form=CustomAuthenticationForm,
-    ), name='auth_login'),
-    url(r'^logout/$', user.UserLogoutView.as_view(), name='auth_logout'),
-    url(r'^password/change/$', auth_views.PasswordChangeView.as_view(
-        template_name='registration/password_change_form.html'
-    ), name='password_change'),
-    url(r'^password/change/done/$', auth_views.PasswordChangeDoneView.as_view(
-        template_name='registration/password_change_done.html',
-    ), name='password_change_done'),
-    url(r'^password/reset/$',auth_views.PasswordResetView.as_view(
-        template_name='registration/password_reset.html',
-        html_email_template_name='registration/password_reset_email.html',
-        email_template_name='registration/password_reset_email.txt',
-    ), name='password_reset'),
-    url(r'^password/reset/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
-        auth_views.PasswordResetConfirmView.as_view(
-            template_name='registration/password_reset_confirm.html',
-        ), name='password_reset_confirm'),
-    url(r'^password/reset/complete/$', auth_views.PasswordResetCompleteView.as_view(
-        template_name='registration/password_reset_complete.html',
-    ), name='password_reset_complete'),
-    url(r'^password/reset/done/$', auth_views.PasswordResetDoneView.as_view(
-        template_name='registration/password_reset_done.html',
-    ), name='password_reset_done'),
-    url(r'^social/error/$', register.social_auth_error, name='social_auth_error'),
-]
+#register_patterns = [
+#    url(r'^activate/complete/$',
+#        TitledTemplateView.as_view(template_name='registration/activation_complete.html',
+#                                   title='Activation Successful!'),
+#        name='registration_activation_complete'),
+#    # Activation keys get matched by \w+ instead of the more specific
+#    # [a-fA-F0-9]{40} because a bad activation key should still get to the view;
+#    # that way it can return a sensible "invalid key" message instead of a
+#    # confusing 404.
+#    url(r'^activate/(?P<activation_key>\w+)/$',
+#        ActivationView.as_view(title='Activation key invalid'),
+#        name='registration_activate'),
+#    url(r'^register/$',
+#        RegistrationView.as_view(title='Register'),
+#        name='registration_register'),
+#    url(r'^register/complete/$',
+#        TitledTemplateView.as_view(template_name='registration/registration_complete.html',
+#                                   title='Registration Completed'),
+#        name='registration_complete'),
+#    url(r'^register/closed/$',
+#        TitledTemplateView.as_view(template_name='registration/registration_closed.html',
+#                                   title='Registration not allowed'),
+#        name='registration_disallowed'),
+#    url(r'^login/$', auth_views.LoginView.as_view(
+#        template_name='registration/login.html',
+#        extra_context={'title': _('Login')},
+#        authentication_form=CustomAuthenticationForm,
+#    ), name='auth_login'),
+#    url(r'^logout/$', user.UserLogoutView.as_view(), name='auth_logout'),
+#    url(r'^password/change/$', auth_views.PasswordChangeView.as_view(
+#        template_name='registration/password_change_form.html'
+#    ), name='password_change'),
+#    url(r'^password/change/done/$', auth_views.PasswordChangeDoneView.as_view(
+#        template_name='registration/password_change_done.html',
+#    ), name='password_change_done'),
+#    url(r'^password/reset/$',auth_views.PasswordResetView.as_view(
+#        template_name='registration/password_reset.html',
+#        html_email_template_name='registration/password_reset_email.html',
+#        email_template_name='registration/password_reset_email.txt',
+#    ), name='password_reset'),
+#    url(r'^password/reset/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
+#        auth_views.PasswordResetConfirmView.as_view(
+#            template_name='registration/password_reset_confirm.html',
+#        ), name='password_reset_confirm'),
+#    url(r'^password/reset/complete/$', auth_views.PasswordResetCompleteView.as_view(
+#        template_name='registration/password_reset_complete.html',
+#    ), name='password_reset_complete'),
+#    url(r'^password/reset/done/$', auth_views.PasswordResetDoneView.as_view(
+#        template_name='registration/password_reset_done.html',
+#    ), name='password_reset_done'),
+#    url(r'^social/error/$', register.social_auth_error, name='social_auth_error'),
+#]
 
 
 def exception(request):
